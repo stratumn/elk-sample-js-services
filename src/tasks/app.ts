c4a4c7fd1f03c836322f183b69fc60dbceb494b8
t-bast <bastien@stratumn.com>
2019-01-24 16:23:09 +0100

    Setup project. Install and configure express and typescript. Create two frontend services.

---
+++ src/tasks/app.ts
@@ -0,0 +1,18 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+class App {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  private config = () => {
+    // support application/json type post data
+    this.app.use(bodyParser.json());
+  };
+}
+
+export default new App().app;
