388d3b7b224d974f69e0203a331c567f03b2469b
t-bast <bastien@stratumn.com>
2019-01-29 10:38:32 +0100

    Add APM automatic instrumentation

--- src/tasks/server.ts
+++ src/tasks/server.ts
@@ -1,3 +1,9 @@
+import * as apm from 'elastic-apm-node';
+apm.start({
+  serviceName: process.env.ELASTIC_APM_SERVICE_NAME || 'task-frontend',
+  serverUrl: process.env.ELASTIC_APM_SERVER_URL || 'http://localhost:8200'
+});
+
 import app from './app';
 import { PORT } from './const';
 
--- src/users/server.ts
+++ src/users/server.ts
@@ -1,3 +1,9 @@
+import * as apm from 'elastic-apm-node';
+apm.start({
+  serviceName: process.env.ELASTIC_APM_SERVICE_NAME || 'user-frontend',
+  serverUrl: process.env.ELASTIC_APM_SERVER_URL || 'http://localhost:8200'
+});
+
 import app from './app';
 import { PORT } from './const';
 
