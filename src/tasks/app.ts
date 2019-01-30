cf81859ee9a074496526a7df6dd60176d0f71509
t-bast <bastien@stratumn.com>
2019-01-30 10:31:35 +0100

    Add elastic-apm headers to cross-origin whitelist

--- src/tasks/app.ts
+++ src/tasks/app.ts
@@ -19,7 +19,7 @@ class App {
       res.header('Access-Control-Allow-Origin', '*');
       res.header(
         'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
+        'Origin, X-Requested-With, Content-Type, Accept, elastic-apm-traceparent'
       );
       next();
     });
